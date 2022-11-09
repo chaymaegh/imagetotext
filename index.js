1e008963037820a5f42e08e45f031f8bf20a7eee
chaymaegh <94975733+chaymaegh@users.noreply.github.com>
2022-11-09 13:38:01 +0100

    Simple Convert image to text

---
+++ index.js
@@ -0,0 +1,35 @@
+const fileSelector = document.querySelector("input");
+const start = document.querySelector("button");
+const img = document.querySelector("img");
+const progress = document.querySelector(".progress");
+const textarea = document.querySelector("textarea");
+
+
+
+// show image on upload
+
+fileSelector.onchange = () => {
+    var file = fileSelector.files[0];
+    var imgUrl = window.URL.createObjectURL(new Blob([file],{type : "image/jpg"}));
+    img.src= imgUrl;
+}
+
+//start text recognition
+
+start.onclick = () =>{
+    textarea.innerHTML= "";
+    const rec = new Tesseract.TesseractWorker();
+    rec.recognize(fileSelector.files[0])
+    .progress(function (response){
+       if(response.status == "recognizing text"){
+        progress.innerHtml = response.status + '   ' + response.progress;
+       }
+       else{
+        progress.innerHTML = response.status;
+       }
+    })
+    .then(function(data){
+        textarea.innerHTML = data.text;
+        progress.innerHTML = "Done" 
+    })
+}
